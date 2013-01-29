80b73cd5e2675cded536d47b9fc57c6a3cdfcf00
woxie <wox2@seznam.cz>
2013-01-29 08:14:57 +0100

    Fixed bug in test component added comments

--- src/eu/collectionspro/mwe/TestComponent.java
+++ src/eu/collectionspro/mwe/TestComponent.java
@@ -24,7 +24,7 @@ public class TestComponent extends AbstractWorkflowComponent{
 	protected List<String> inputUris;
 	protected List<String> comparisonUris;
 
-	//tested transformation URI
+	/** tested transformation URI **/
 	protected String transformationFile;
 	/**
 	 * qvt output dir parent
@@ -36,11 +36,11 @@ public class TestComponent extends AbstractWorkflowComponent{
 	 */
 	protected String testDescription;
 	
-	//hidded Comparators
+	//hidden Comparators
 	protected List<ModelComparator> modelComparators;
-	private final String INPUT_PREFIX = "INPUT_";
-	private final String RESULT_PREFIX = "RESULT_";
-	private final String COMPARISON_PREFIX = "COMPARISON_";
+	protected final String INPUT_PREFIX = "INPUT_";
+	protected final String RESULT_PREFIX = "RESULT_";
+	protected final String COMPARISON_PREFIX = "COMPARISON_";
 	
 	public TestComponent(){
 		inputUris = new ArrayList<String>();
@@ -66,9 +66,17 @@ public class TestComponent extends AbstractWorkflowComponent{
 		}
 	}
 	
+	/**
+	 *  Stores slots' contents into files, skips slots with transformation ""
+	 * @param transformationList origin model transformation lists 
+	 * @param slotPrefix prefix of slots to be stored
+	 * @param ctx
+	 * @param monitor
+	 * @param issues
+	 */
 	protected void storeSlots(List <String> transformationList, String slotPrefix, 
 			WorkflowContext ctx, ProgressMonitor monitor, Issues issues){
-		for( int i = 0 ; i < inputUris.size() ; i++){
+		for( int i = 0 ; i < transformationList.size() ; i++){
 			if(!transformationList.get(i).equals("")){
 				String slot = slotPrefix + i;
 				Writer slotWriter = new Writer();
@@ -81,7 +89,7 @@ public class TestComponent extends AbstractWorkflowComponent{
 			}
 		}
 	}
-	
+		
 	@Override
 	protected void invokeInternal(WorkflowContext ctx, ProgressMonitor monitor,
 			Issues issues) {
@@ -90,13 +98,13 @@ public class TestComponent extends AbstractWorkflowComponent{
 		fulfillSlots(inputUris, INPUT_PREFIX, ctx, monitor, issues);
 		storeSlots(inputUris, INPUT_PREFIX, ctx, monitor, issues);
 		
-		//adding inout slots into test transformation
+		//adding input slots into test transformation
 		for( int i = 0 ; i < inputUris.size() ; i++){
 			String inputSlot = INPUT_PREFIX + i;
 			testTransformationExecuter.addInputSlot(inputSlot);
 		}
 		
-		//adding inout slot into test transformation
+		//adding output slots into test transformation
 		for(int i = 0 ; i < comparisonUris.size() ; i++){
 			String resultSlot = RESULT_PREFIX + i;
 			testTransformationExecuter.addOutputSlot(resultSlot);
@@ -131,14 +139,26 @@ public class TestComponent extends AbstractWorkflowComponent{
 		System.out.println("Testing " + testDescription + " finisthed\n");
 	}
 
+	/** 
+	 * Adds input uri to be used by test transformation. 
+	 */
 	public void addQvtInput(final String inputUri){
 		this.inputUris.add(inputUri);
 	}
 	
+	/** 
+	 * Adds expected result uri to be compared after test transformation is performed. Order(types) 
+	 * and amount must equal to transformation outputs. Uri "" can be used when no comparison needed
+	 * @param comparisonUri
+	 */
 	public void addQvtComparison(final String comparisonUri){
 		this.comparisonUris.add(comparisonUri);
 	}
 	
+	/**
+	 *  Sets location where output xmi files should  be stored into
+	 * @param uri
+	 */
 	public void setOutputParentUri(String uri){
 		outputParentUri = uri;
 	}
@@ -150,12 +170,16 @@ public class TestComponent extends AbstractWorkflowComponent{
 		testDescription = description;
 	}
 	
+	/** sets uri of tested transformation **/
 	public void setTransformationFile(String transformationUri){
 		transformationFile = transformationUri;
 	}
 
 	
 	@Override
+	/**
+	 *  checks configuration of Component. Doesn't check configurations of embedded components
+	 */
 	public void checkConfiguration(Issues issues) {
 		if(transformationFile == null || transformationFile.equals("")){
 			issues.addError("Missing tested transformation URI");
